e7c1d0967b70f45672ac66e579372bc615e31b32
anjabruestl <anja.bruestl@s.hakmistelbach.ac.at>
2024-11-07 12:27:25 +0100

    07.11.2024
    Kontakt und Q&A

--- templates/kontakt.php
+++ templates/kontakt.php
@@ -39,11 +39,6 @@ ini_set('display_errors','On');
             <br><br>
           <h5 class="card-title">Adresse</h5>
           <p class="card-text ">2130 Mistelbach, Edmund-Freibauer-Promenade 1</p>
-          <a href="https://www.google.at/maps/place/Edmund+Freibauer-Promenade+1,+2130+Mistelbach/@48.5711588,16.5645224,17z/data=!3m1!4b1!4m6!3m5!1s0x476d249fe3477e8d:0xec82cd406283da95!8m2!3d48.5711553!4d16.5670973!16s%2Fg%2F11y28xld1z?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#00AAFF" class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-              <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707"/>
-            </svg>
-          </a>
         </div>
       </div>
     </div>
@@ -57,12 +52,8 @@ ini_set('display_errors','On');
         </svg>
           <br><br>
         <h5 class="card-title">Telefon</h5>
-        <p class="card-text ">06508749235</p>
-        <a href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#00AAFF" class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-            <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707"/>
-          </svg>
-        </a>
+        <p class="card-text ">+43 2572/2305-350</p>
+        
       </div>
     </div>
   </div>
@@ -76,12 +67,7 @@ ini_set('display_errors','On');
         </svg>
           <br><br>
         <h5 class="card-title">E-Mail</h5>
-        <p class="card-text ">anja.bruestl@s.hakmistelbach.ac.at</p>
-        <a href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#00AAFF" class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-            <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707"/>
-          </svg>
-        </a>
+        <p class="card-text">service@aposupply.digbizmistelbach.info</p>
       </div>
     </div>
   </div>
@@ -93,111 +79,107 @@ ini_set('display_errors','On');
 <div class="row">
   <div class="col-sm-6 mb-3 mb-sm-0">
     <div class="card"> 
-      <div class="card-body text-center mb-3">
-        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-map-fill" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.598-.49L10.5.99 5.598.01a.5.5 0 0 0-.196 0l-5 1A.5.5 0 0 0 0 1.5v14a.5.5 0 0 0 .598.49l4.902-.98 4.902.98a.5.5 0 0 0 .196 0l5-1A.5.5 0 0 0 16 14.5zM5 14.09V1.11l.5-.1.5.1v12.98l-.402-.08a.5.5 0 0 0-.196 0zm5 .8V1.91l.402.08a.5.5 0 0 0 .196 0L11 1.91v12.98l-.5.1z"/>
-        </svg>
-        <br><br>
-        <h5 class="card-title">Google Maps</h5>
-        <br>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2640.080498525776!2d16.567565!3d48.570007!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d23601a955245%3A0xeca4453950a6cafb!2sBundeshandelsakademie%20und%20Bundeshandelsschule!5e0!3m2!1sde!2sat!4v1730705553427!5m2!1sde!2sat" width="570" height="420" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2640.080498525776!2d16.567565!3d48.570007!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d23601a955245%3A0xeca4453950a6cafb!2sBundeshandelsakademie%20und%20Bundeshandelsschule!5e0!3m2!1sde!2sat!4v1730705553427!5m2!1sde!2sat" width="636" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>  
   </div>  <!-- ende col -->
 
-  <!-- Kontakt -->
+    <!-- Kontakt card -->
     <div class="col-sm-6 mb-3 mb-sm-0">
       <div class="card"> 
         <div class="card-body text-center mb-3">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-            <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
-          </svg>
-          <br><br>
-          <h5 class="card-title">Kontakt</h5>
           <br>
 
-  <!-- Formular -->
-        <div class="input-group mb-3">
-          <span class="input-group-text">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
-              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
-            </svg>
-          </span>
-          <form class="form-floating">
-            <input type="email" class="form-control" id="floatingInputValue" placeholder="name@test.com">
-            <label for="floatingInputValue">E-Mail</label>
-          </form>
-        </div>
-        <br><br>
-
-        <div class="input-group mb-3">
-          <span class="input-group-text">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-fill" viewBox="0 0 16 16">
-              <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A6 6 0 0 1 5 6.708V2.277a3 3 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354"/>
-            </svg>
-          </span>
-          <form class="form-floating">
-            <input type="text" class="form-control" id="floatingInputValue" placeholder="name@example.com" >
-            <label for="floatingInputValue">Betreff</label>
-          </form>
-        </div>
-        <br><br>
-
-        <div class="input-group mb-3">
-          <span class="input-group-text">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text-fill" viewBox="0 0 16 16">
-              <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1m-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1m0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1"/>
-            </svg>
-          </span>
-          <form class="form-floating">
-          <textarea class="form-control" placeholder="Schreibe eine Nachricht!" id="floatingTextarea2" style="height: 165px"></textarea>
-          <label for="floatingTextarea2">Nachricht</label>
-        </div>
+      <!-- Formular -->
+      <div class="input-group mb-3">
+        <span class="input-group-text">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+        </svg>
+        </span>
+        <form class="form-floating">
+          <input type="text" class="form-control" id="floatingInputValue" placeholder="0">
+          <label for="floatingInputValue">Vor- und Nachname</label>
+        </form>
       </div>
+      <br>
+
+      <!-- Email -->
+      <div class="input-group mb-3">
+        <span class="input-group-text">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+          </svg>
+        </span>
+        <form class="form-floating">
+          <input type="email" class="form-control" id="floatingInputValue" placeholder="name@test.com">
+          <label for="floatingInputValue">E-Mail</label>
+        </form>
+      </div>
+      <br>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-fill" viewBox="0 0 16 16">
+            <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A6 6 0 0 1 5 6.708V2.277a3 3 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354"/>
+          </svg>
+        </span>
+        <form class="form-floating">
+          <input type="text" class="form-control" id="floatingInputValue" placeholder="0" >
+          <label for="floatingInputValue">Betreff</label>
+        </form>
+      </div>
+      <br>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text-fill" viewBox="0 0 16 16">
+            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1m-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1m0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1"/>
+          </svg>
+        </span>
+        <form class="form-floating">
+          <textarea class="form-control" placeholder="Schreibe eine Nachricht!" id="floatingTextarea2" style="height: 120px"></textarea>
+          <label for="floatingTextarea2">Nachricht</label>
+        </form>
+      </div>
+      <br>
+
+
+<!-- button nachricht senden Eberhart fragen!! -->
+      <button class="btn btn-primary d-grid gap-2 col-6 mx-auto" type="submit" name="login-button"  width="100">Nachricht senden</button>
+      
+
+
+
+    </div> <!-- ende card -->
   </div> <!-- ende col -->
   <br><br><br><br>
 </div> <!-- ende row -->
 
 
+<!-- Form für Nachricht senden EBERHART FRAGEN!!! -->
+<?php 
+  if(isset($_GET['korrekt'])){
+    $hans = $_GET['korrekt'];
+    echo $hans;
+  }
+?>
 
-<!-- css -->
-<style>
-.card{
-  border: #ffffff; 
-}
-
-
-
-</style>
-
-
-
-
-
-
-
-
-
-
-
-
-
+<form method="POST" action="functions/login2.php">
+  <div class="mb-3">
+    <label for="em" class="form-label">E-Mail</label>
+    <input type="email" name="email" class="form-control" id="em" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="pw" class="form-label">Passwort</label>
+    <input type="password" name="passwort" class="form-control" id="pw">
+  </div>
+  <div class="card-footer">
+    <button type="submit" name="login-button" class="btn btn-primary">Login</button>
+  </div>
+</form>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+<!-- Css -->
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
@@ -223,6 +205,21 @@ body {
         background-position: 0% 50%;
     }
 }
+
+.card{
+  border: #ffffff; 
+}
+
+.card-text{
+  font-size: 15px;
+  font-weight: 400;
+}
+
+.card-title{
+  font-size: 20px;
+  font-weight: 600;
+}
+
 </style>
 
 </html>
