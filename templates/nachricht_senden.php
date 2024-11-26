09901cb9bff21734914bbbca653c77474fc21815
anjabruestl <anja.bruestl@s.hakmistelbach.ac.at>
2024-11-26 17:19:37 +0100

    26.11.2024
    Nachrichtseite fertig

--- templates/nachricht_senden.php
+++ templates/nachricht_senden.php
@@ -12,9 +12,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers = "From: $email";
 
     if (mail($to, $subject, $body, $headers)) {
-        echo "Nachricht erfolgreich gesendet!";
+      ?>
+        <div class="alert alert-info" role="alert">
+          Ihre Nachricht ist erfolgreich bei uns eingegangen.
+        </div>
+      <?php
     } else {
-        echo "Es gab ein Problem beim Senden der Nachricht.";
+      ?>
+        <div class="alert alert-info" role="alert">
+          Es gab ein Problem beim Senden der Nachricht. Versuchen Sie es später erneut!
+        </div>
+      <?php
     }
 }
 ?>
@@ -24,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AGB
+    <title>Dankeschön
     </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="icon" href="logo_richtig.png" type="image/x-icon">
@@ -39,16 +47,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ?>
     <div class="container">
 
-    <br>
-    <h1 style="font-weight: 700; letter-spacing: 0.5px; font-size: 45px;">Danke für dein Feedback!</h1>
+    <h1 style="font-weight: 700; letter-spacing: 0.5px; font-size: 45px;">Danke für Ihr Feedback! &#129321; </h1>
     <br>
 
 	<div class="alert alert-info" role="alert">
-	  Ihre Nachricht ist erfolgreich bei uns eingegangen. Vielen Dank!
+	  Ihre Nachricht ist erfolgreich bei uns eingegangen.
 	</div>
+		
+<p style="font-weight: 500; font-size: 18px;">Wir nehmen uns Ihre Nachrichten sehr zu Herzen und werden versuchen, dementsprechend darauf zu reagieren. Bitte haben Sie Verständnis, wenn es einmal zu einer Verzögerung unserer Antwort kommen könnte. Danke! </p>
 
 		<br>
-<a href="templates/kontakt.php" class="btn umfrage_btn" tabindex="-1" role="button" aria-disabled="true">zurück zur Website</a>
+<a href="templates/kontakt.php" class="btn umfrage_btn" tabindex="-1" role="button" aria-disabled="true">
+	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+  		<path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+	</svg> zurück zur Website</a>
 
 		
 	</body>
@@ -95,10 +107,18 @@ body {
 .alert-info{
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0,0,0,0.6);
-  background-color: rgba(0,170,255,0.3);
+  background-color: rgba(0,170,255,0.6);
   font-weight: 500;
   color: black;
   letter-spacing: 0.5px;
+  border-color: #00aaff;
+  font-size: 18px;
+	
+  animation: sweep 1.5s ease-in-out;}
+
+@keyframes sweep {
+  0%    {opacity: 0; margin-left: -20px}
+  100%  {opacity: 1; margin-left: 0px}
 }
 	  
   </style>
