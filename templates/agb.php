35be944dcdb54db294bdb7d0a6b14919a737e1f3
anjabruestl <anja.bruestl@s.hakmistelbach.ac.at>
2024-11-26 12:58:45 +0100

    26.11.2024
    website online

--- templates/agb.php
+++ templates/agb.php
@@ -14,6 +14,7 @@ ini_set('display_errors','On');
     <title>AGB
     </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="icon" href="logo_richtig.png" type="image/x-icon">
     <link href="../functions/mystyle.css"rel="stylesheet">
     <base href="/aposupply/">
 </head>
