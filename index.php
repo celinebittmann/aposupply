ae3b846a2a0fcc8df35faf56ee472b6fe9a2a8c9
anjabruestl <anja.bruestl@s.hakmistelbach.ac.at>
2024-11-26 12:50:12 +0100

    logo

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ ini_set('display_errors','On');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Apo-Supply</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="icon" href="tablets.png" type="image/x-icon">
+    <link rel="icon" href="logo_medi.png" type="image/x-icon">
     <link href="functions/mystyle.css?v=1.0" rel="stylesheet" type="text/css" >
   </head>
   <br><br><br>
