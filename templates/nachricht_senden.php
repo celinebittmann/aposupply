b0a0bfa2175c37a9092df5dfa7d2798870bc4e86
anjabruestl <anja.bruestl@s.hakmistelbach.ac.at>
2024-11-26 14:20:07 +0100

    26.11.2024
    nachricht senden seite angefangen zu designen

--- templates/nachricht_senden.php
+++ templates/nachricht_senden.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $to = "service@aposupply.digbizmistelbach.info"; 
     $subject = "Neue Nachricht von Apo-Supply";
-    $body = "Name: $name\n E-Mail: $email\nNachricht:\n$message";
+    $body = "Name: $name\n E-Mail: $email\n Betreff: $betreff\n Nachricht:\n$message";
 
     $headers = "From: $email";
 
@@ -18,3 +18,87 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AGB
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="icon" href="logo_richtig.png" type="image/x-icon">
+    <link href="../functions/mystyle.css"rel="stylesheet">
+    <base href="/web/">
+</head>
+<br><br><br>
+  <body>
+    <?php
+    //  require __DIR__.'/../includes.php'; 
+    require "navbar.php";
+    ?>
+    <div class="container">
+
+    <br>
+    <h1 style="font-weight: 700; letter-spacing: 0.5px; font-size: 45px;">Danke für dein Feedback!</h1>
+    <br>
+
+	<div class="alert alert-info" role="alert">
+	  Ihre Nachricht ist erfolgreich bei uns eingegangen. Vielen Dank!
+	</div>
+
+		<br>
+<a href="templates/kontakt.php" class="btn umfrage_btn" tabindex="-1" role="button" aria-disabled="true">zurück zur Website</a>
+
+		
+	</body>
+</html>
+
+  
+  <style>
+body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background: linear-gradient(90deg, #e6f9ff, #d7f8fe, #6ae8fe, #e6f9ff, #d7f8fe);
+    background-size: 200% 200%;
+    animation: gradientMove 12s linear infinite;
+}
+
+@keyframes gradientMove {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+	  
+.umfrage_btn{
+  border-radius: 30px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.6);
+  background-color: rgba(0,170,255,0.3);
+  font-weight: 700;
+  letter-spacing: 0.5px;
+}
+
+.umfrage_btn:hover{
+  border-radius: 30px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.8);
+  transform: translateY(0) scale(1.07);
+  background-color: #00aaff;
+}
+	  
+.alert-info{
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.6);
+  background-color: rgba(0,170,255,0.3);
+  font-weight: 500;
+  color: black;
+  letter-spacing: 0.5px;
+}
+	  
+  </style>
